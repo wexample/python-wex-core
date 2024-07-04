8bc5fb3e6938456a723aa7cb77a20553f4b8cb43
weeger <romain.weeger@gmail.com>
2024-07-04 17:10:17 +0200

    Call basics

--- wexample_wex_core/utils/kernel.py
+++ wexample_wex_core/utils/kernel.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from typing import Dict
 
 from pydantic import BaseModel
@@ -9,6 +10,7 @@ from wexample_prompt.io_manager import IOManager
 class Kernel(BaseModel):
     def __init__(self, entrypoint_path: str) -> None:
         super().__init__()
+        self._sys_argv: list[str] = sys.argv.copy()
 
         root_path = os.path.dirname(os.path.realpath(entrypoint_path)) + os.sep
 
@@ -17,3 +19,14 @@ class Kernel(BaseModel):
         }
 
         self.io = IOManager()
+
+    def call(self) -> None:
+        """
+        Main entrypoint from bash call.
+        May never be called by an internal script.
+        :return:
+        """
+
+        # No arg found except process id
+        if not len(self._sys_argv) > 2:
+            return
