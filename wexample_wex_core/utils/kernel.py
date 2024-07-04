f298bdd6ef8c8f8bb9882ccee899c170f3c50a77
weeger <romain.weeger@gmail.com>
2024-07-04 17:06:04 +0200

    Kernel basics

--- wexample_wex_core/utils/kernel.py
+++ wexample_wex_core/utils/kernel.py
@@ -1,5 +1,19 @@
+import os
+from typing import Dict
+
 from pydantic import BaseModel
+
+from wexample_prompt.io_manager import IOManager
 
 
 class Kernel(BaseModel):
-    pass
+    def __init__(self, entrypoint_path: str) -> None:
+        super().__init__()
+
+        root_path = os.path.dirname(os.path.realpath(entrypoint_path)) + os.sep
+
+        self._path: Dict[str, str] = {
+            "root": root_path,
+        }
+
+        self.io = IOManager()
