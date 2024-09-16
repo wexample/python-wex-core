d587878e7f151e823234c7758c5a29e899b6f4c6
weeger <romain.weeger@gmail.com>
2024-09-16 10:35:34 +0200

    Use abstract

--- wexample_wex_core/utils/kernel.py
+++ wexample_wex_core/utils/kernel.py
@@ -1,33 +1,24 @@
-import os
-import sys
-from typing import Dict, Optional
+from typing import Type, Any
 
+from wexample_filestate.file_state_manager import FileStateManager
 from wexample_helpers_app.utils.abstract_kernel import AbstractKernel
 from wexample_wex_core.core.file.KernelDirectoryStructure import KernelDirectoryStructure
+from wexample_helpers_app.utils.mixins.command_line_kernel import CommandLineKernel
 
 
-class Kernel(AbstractKernel):
-    directory: Optional[KernelDirectoryStructure]
+class Kernel(AbstractKernel, CommandLineKernel):
+    def __init__(self, /, **data: Any) -> None:
+        super().__init__(**data)
+        self.__init_command_line_kernel()
 
-    def __init__(self, entrypoint_path: str) -> None:
-        super().__init__()
-        self._sys_argv: list[str] = sys.argv.copy()
+    def _get_workdir_state_manager_class(self) -> Type[FileStateManager]:
+        return KernelDirectoryStructure
 
-        root_path = os.path.dirname(os.path.realpath(entrypoint_path)) + os.sep
-
-        self._path: Dict[str, str] = {
-            "root": root_path,
-        }
-
-        self.directory = KernelDirectoryStructure.create_from_path(
-            path=root_path,
-        )
 
     def call(self) -> None:
         """
         Main entrypoint from bash call.
         May never be called by an internal script.
-        :return:
         """
 
         # No arg found except process id
