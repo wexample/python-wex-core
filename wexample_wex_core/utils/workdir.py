9ab1a5c75638e82594a8059216d0bf47b8a00a89
weeger <romain.weeger@gmail.com>
2024-10-30 01:13:56 +0100

    Nesting works and test pass

--- wexample_wex_core/utils/workdir.py
+++ wexample_wex_core/utils/workdir.py
@@ -1,29 +1,14 @@
-from wexample_filestate.const.enums import DiskItemType
-from wexample_filestate.const.types import StateItemConfig
+from wexample_config.const.types import DictConfig
 from wexample_filestate.file_state_manager import FileStateManager
+
 from wexample_helpers.const.types import *
-from wexample_wex_core.const.globals import WORKDIR_SETUP_DIR
 
 
 class Workdir(FileStateManager):
-
-    def build_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
-        config = super().build_config(config)
+    def prepare_value(self, config: Optional[DictConfig] = None) -> DictConfig:
+        config = super().prepare_value(config)
 
         if "children" not in config:
             config["children"] = []
 
-        config["children"].append(self.build_setup_config())
-
-        return config or {}
-
-    def build_setup_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
-        return {
-            "children": [],
-            "name": WORKDIR_SETUP_DIR,
-            "type": DiskItemType.DIRECTORY,
-            "should_exist": True
-        }
-
-
-Workdir.model_rebuild()
+        return config
