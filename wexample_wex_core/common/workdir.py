917ff48abc39c81d17502e0bef163b044817fe19
weeger <romain.weeger@gmail.com>
2025-01-10 20:36:37 +0100

    Add env dir

--- wexample_wex_core/common/workdir.py
+++ wexample_wex_core/common/workdir.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from wexample_config.const.types import DictConfig
 from wexample_filestate.file_state_manager import FileStateManager
 
@@ -6,9 +8,18 @@ from wexample_helpers.const.types import *
 
 class Workdir(FileStateManager):
     def prepare_value(self, config: Optional[DictConfig] = None) -> DictConfig:
+        from wexample_wex_core.const.globals import WORKDIR_SETUP_DIR
+        from wexample_filestate.const.disk import DiskItemType
+
         config = super().prepare_value(config)
 
         if "children" not in config:
             config["children"] = []
+
+        config["children"].append({
+            "name": WORKDIR_SETUP_DIR,
+            'type': DiskItemType.DIRECTORY,
+            'should_exist': True,
+        })
 
         return config
