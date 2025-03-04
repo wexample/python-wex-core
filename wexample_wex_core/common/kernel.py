8bb56de375c16e819b9f23531c34215795f4b657
weeger <romain.weeger@gmail.com>
2025-03-04 07:39:01 +0100

    Load env file

--- wexample_wex_core/common/kernel.py
+++ wexample_wex_core/common/kernel.py
@@ -1,11 +1,10 @@
 from typing import Any
 
-from wexample_helpers.classes.mixin.has_env_keys import HasEnvKeys
 from wexample_app.common.abstract_kernel import AbstractKernel
 from wexample_app.common.mixins.command_line_kernel import CommandLineKernel
 
 
-class Kernel(AbstractKernel, HasEnvKeys, CommandLineKernel):
+class Kernel(AbstractKernel, CommandLineKernel):
     def model_post_init(self, __context: Any) -> None:
         super().model_post_init(__context)
         self._init_command_line_kernel()
