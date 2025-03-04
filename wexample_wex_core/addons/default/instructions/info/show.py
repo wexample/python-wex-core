67147ea03b772148a4e4db013d2d58e3fd0263ca
weeger <romain.weeger@gmail.com>
2025-03-04 20:34:09 +0100

    Move info

---
+++ wexample_wex_core/addons/default/instructions/info/show.py
@@ -0,0 +1,26 @@
+from wexample_wex_core.common.kernel import Kernel
+
+
+def default__info__show(
+        kernel: "Kernel",
+) -> None:
+    from wexample_app.const.globals import ENV_VAR_NAME_APP_ENV
+
+    kernel.io.properties(
+        title="General",
+        properties={
+            "Location": kernel.workdir.get_resolved(),
+            "Environment": kernel.get_env_parameter(ENV_VAR_NAME_APP_ENV),
+            "Arguments": kernel._sys_argv,
+        }
+    )
+
+    kernel.io.properties(
+        title="Resolvers",
+        properties=kernel.get_resolvers()
+    )
+
+    kernel.io.properties(
+        title="Runners",
+        properties=kernel.get_runners()
+    )
