b1657d5b00158d79224bb7c5c29e9dbab94c762a
weeger <romain.weeger@gmail.com>
2025-03-04 16:04:11 +0100

    Doc

--- wexample_wex_core/addons/default/instructions/version/increment.py
+++ wexample_wex_core/addons/default/instructions/version/increment.py
@@ -1,13 +1,14 @@
-from wexample_wex_addon_default.const.types import (UPGRADE_TYPE_MINOR)
-from wexample_wex_addon_default.helpers.version import version_increment
+from wexample_helpers.const.types import UPGRADE_TYPE_MINOR
+from wexample_helpers.helpers.version import version_increment
+from wexample_wex_core.common.kernel import Kernel
 
 
 def default__version__increment(
-    kernel: "Kernel",
-    version: str,
-    type: str = UPGRADE_TYPE_MINOR,
-    increment: int = 1,
-    build: bool = False,
+        kernel: "Kernel",
+        version: str,
+        type: str = UPGRADE_TYPE_MINOR,
+        increment: int = 1,
+        build: bool = False,
 ) -> str:
     return version_increment(
         version=version,
