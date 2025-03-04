de99dae495580a464113adb1d9892579dfb35101
weeger <romain.weeger@gmail.com>
2025-03-04 07:21:17 +0100

    Set env management and wrap errors

--- wexample_wex_core/common/kernel.py
+++ wexample_wex_core/common/kernel.py
@@ -1,22 +1,11 @@
 from typing import Any
 
+from wexample_helpers.classes.mixin.has_env_keys import HasEnvKeys
 from wexample_app.common.abstract_kernel import AbstractKernel
 from wexample_app.common.mixins.command_line_kernel import CommandLineKernel
 
 
-class Kernel(AbstractKernel, CommandLineKernel):
+class Kernel(AbstractKernel, HasEnvKeys, CommandLineKernel):
     def model_post_init(self, __context: Any) -> None:
         super().model_post_init(__context)
         self._init_command_line_kernel()
-
-    def _get_command_resolvers(self) -> list[Type["AbstractCommandResolver"]]:
-        from wexample_wex_core.common.resolver.service_command_resolver import ServiceCommandResolver
-
-        return [
-            ServiceCommandResolver,
-        ]
-
-    def _get_workdir_state_manager_class(self) -> Type["FileStateManager"]:
-        from wexample_wex_core.common.file.kernel_directory_structure import KernelDirectoryStructure
-
-        return KernelDirectoryStructure
