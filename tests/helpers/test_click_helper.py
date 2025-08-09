b6e30772d0cce2ed4b6964f064606996df703da7
weeger <romain.weeger@gmail.com>
2025-08-09 11:08:53 +0200

    Comment dead code

--- tests/helpers/test_click_helper.py
+++ tests/helpers/test_click_helper.py
@@ -1,124 +1,124 @@
-import pytest
-import click
-
-from wexample_wex_core.helpers.click_helper import (
-    click_args_convert_dict_to_long_names_dict,
-    click_args_convert_to_dict,
-    click_args_convert_dict_to_args,
-)
-
-
-@pytest.fixture
-def test_command():
-    """Create a test command for use in tests"""
-    @click.command()
-    @click.option('-n', '--name', help='Name option')
-    @click.option('-f', '--flag', is_flag=True, help='Flag option')
-    @click.option('-c', '--count', type=int, help='Count option')
-    def cmd():
-        pass
-    
-    return cmd
-
-
-def test_click_args_convert_dict_to_long_names_dict(test_command):
-    """Test conversion from short to long names in dictionary"""
-    # Test with short names
-    args = {
-        'n': 'test',
-        'f': True,
-        'count': 5
-    }
-    expected = {
-        'name': 'test',
-        'flag': True,
-        'count': 5
-    }
-    assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
-
-    # Test with long names
-    args = {
-        'name': 'test',
-        'flag': True,
-        'count': 5
-    }
-    assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
-
-    # Test with mixed names
-    args = {
-        'name': 'test',
-        'f': True,
-        'count': 5
-    }
-    assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
-
-
-def test_click_args_convert_to_dict(test_command):
-    """Test conversion from args list to dictionary"""
-    # Test with long names
-    args = ['--name', 'test', '--flag', '--count', '5']
-    expected = {
-        'name': 'test',
-        'flag': True,
-        'count': '5'
-    }
-    assert click_args_convert_to_dict(test_command, args) == expected
-
-    # Test with short names
-    args = ['-n', 'test', '-f', '-c', '5']
-    expected = {
-        'n': 'test',
-        'f': True,
-        'c': '5'
-    }
-    assert click_args_convert_to_dict(test_command, args) == expected
-
-    # Test with mixed names
-    args = ['--name', 'test', '-f', '--count', '5']
-    expected = {
-        'name': 'test',
-        'f': True,
-        'count': '5'
-    }
-    assert click_args_convert_to_dict(test_command, args) == expected
-
-
-def test_click_args_convert_dict_to_args(test_command):
-    """Test conversion from dictionary to args list"""
-    # Test with all options
-    args = {
-        'name': 'test',
-        'flag': True,
-        'count': 5
-    }
-    result = click_args_convert_dict_to_args(test_command, args)
-    assert '--name' in result
-    assert 'test' in result
-    assert '--flag' in result
-    assert '--count' in result
-    assert '5' in result
-
-    # Test with some options
-    args = {
-        'name': 'test',
-        'count': 5
-    }
-    result = click_args_convert_dict_to_args(test_command, args)
-    assert '--name' in result
-    assert 'test' in result
-    assert '--count' in result
-    assert '5' in result
-    assert '--flag' not in result
-
-    # Test with flag set to False
-    args = {
-        'name': 'test',
-        'flag': False,
-        'count': 5
-    }
-    result = click_args_convert_dict_to_args(test_command, args)
-    assert '--name' in result
-    assert 'test' in result
-    assert '--count' in result
-    assert '5' in result
-    assert '--flag' not in result
+# import pytest
+# import click
+#
+# from wexample_wex_core.helpers.click_helper import (
+#     click_args_convert_dict_to_long_names_dict,
+#     click_args_convert_to_dict,
+#     click_args_convert_dict_to_args,
+# )
+#
+#
+# @pytest.fixture
+# def test_command():
+#     """Create a test command for use in tests"""
+#     @click.command()
+#     @click.option('-n', '--name', help='Name option')
+#     @click.option('-f', '--flag', is_flag=True, help='Flag option')
+#     @click.option('-c', '--count', type=int, help='Count option')
+#     def cmd():
+#         pass
+#
+#     return cmd
+#
+#
+# def test_click_args_convert_dict_to_long_names_dict(test_command):
+#     """Test conversion from short to long names in dictionary"""
+#     # Test with short names
+#     args = {
+#         'n': 'test',
+#         'f': True,
+#         'count': 5
+#     }
+#     expected = {
+#         'name': 'test',
+#         'flag': True,
+#         'count': 5
+#     }
+#     assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
+#
+#     # Test with long names
+#     args = {
+#         'name': 'test',
+#         'flag': True,
+#         'count': 5
+#     }
+#     assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
+#
+#     # Test with mixed names
+#     args = {
+#         'name': 'test',
+#         'f': True,
+#         'count': 5
+#     }
+#     assert click_args_convert_dict_to_long_names_dict(test_command, args) == expected
+#
+#
+# def test_click_args_convert_to_dict(test_command):
+#     """Test conversion from args list to dictionary"""
+#     # Test with long names
+#     args = ['--name', 'test', '--flag', '--count', '5']
+#     expected = {
+#         'name': 'test',
+#         'flag': True,
+#         'count': '5'
+#     }
+#     assert click_args_convert_to_dict(test_command, args) == expected
+#
+#     # Test with short names
+#     args = ['-n', 'test', '-f', '-c', '5']
+#     expected = {
+#         'n': 'test',
+#         'f': True,
+#         'c': '5'
+#     }
+#     assert click_args_convert_to_dict(test_command, args) == expected
+#
+#     # Test with mixed names
+#     args = ['--name', 'test', '-f', '--count', '5']
+#     expected = {
+#         'name': 'test',
+#         'f': True,
+#         'count': '5'
+#     }
+#     assert click_args_convert_to_dict(test_command, args) == expected
+#
+#
+# def test_click_args_convert_dict_to_args(test_command):
+#     """Test conversion from dictionary to args list"""
+#     # Test with all options
+#     args = {
+#         'name': 'test',
+#         'flag': True,
+#         'count': 5
+#     }
+#     result = click_args_convert_dict_to_args(test_command, args)
+#     assert '--name' in result
+#     assert 'test' in result
+#     assert '--flag' in result
+#     assert '--count' in result
+#     assert '5' in result
+#
+#     # Test with some options
+#     args = {
+#         'name': 'test',
+#         'count': 5
+#     }
+#     result = click_args_convert_dict_to_args(test_command, args)
+#     assert '--name' in result
+#     assert 'test' in result
+#     assert '--count' in result
+#     assert '5' in result
+#     assert '--flag' not in result
+#
+#     # Test with flag set to False
+#     args = {
+#         'name': 'test',
+#         'flag': False,
+#         'count': 5
+#     }
+#     result = click_args_convert_dict_to_args(test_command, args)
+#     assert '--name' in result
+#     assert 'test' in result
+#     assert '--count' in result
+#     assert '5' in result
+#     assert '--flag' not in result
--- tests/helpers/test_version_helper.py
+++ tests/helpers/test_version_helper.py
@@ -1,123 +1,123 @@
-import pytest
-
-from wexample_wex_core.const.types import (
-    VersionDescriptor, UPGRADE_TYPE_MINOR, UPGRADE_TYPE_MAJOR,
-    UPGRADE_TYPE_INTERMEDIATE, UPGRADE_TYPE_ALPHA, UPGRADE_TYPE_BETA
-)
-from wexample_wex_core.helpers.version import (
-    is_greater_than,
-    version_join,
-    version_parse,
-    version_increment
-)
-
-
-def test_version_parse():
-    # Test basic versions
-    assert version_parse("1.2.3") == {
-        "major": 1,
-        "intermediate": 2,
-        "minor": 3,
-        "pre_build_type": None,
-        "pre_build_number": None
-    }
-    
-    # Test pre-release versions
-    assert version_parse("1.2.3-beta.1") == {
-        "major": 1,
-        "intermediate": 2,
-        "minor": 3,
-        "pre_build_type": "beta",
-        "pre_build_number": 1
-    }
-    
-    # Test partial versions
-    assert version_parse("1.2") == {
-        "major": 1,
-        "intermediate": 2,
-        "minor": None,
-        "pre_build_type": None,
-        "pre_build_number": None
-    }
-    
-    # Test invalid versions
-    assert version_parse("invalid") is None
-    assert version_parse("1.2.three") is None
-
-
-def test_version_join():
-    # Test basic version
-    version: VersionDescriptor = {
-        "major": 1,
-        "intermediate": 2,
-        "minor": 3,
-        "pre_build_type": None,
-        "pre_build_number": None
-    }
-    assert version_join(version) == "1.2.3"
-    
-    # Test pre-release version
-    version = {
-        "major": 1,
-        "intermediate": 2,
-        "minor": 3,
-        "pre_build_type": "beta",
-        "pre_build_number": 1
-    }
-    assert version_join(version) == "1.2.3-beta.1"
-
-
-def test_is_greater_than():
-    v1: VersionDescriptor = {
-        "major": 2,
-        "intermediate": 0,
-        "minor": 0,
-        "pre_build_type": None,
-        "pre_build_number": None
-    }
-    v2: VersionDescriptor = {
-        "major": 1,
-        "intermediate": 9,
-        "minor": 9,
-        "pre_build_type": None,
-        "pre_build_number": None
-    }
-    
-    # Test major version comparison
-    assert is_greater_than(v1, v2)
-    assert not is_greater_than(v2, v1)
-    
-    # Test equal versions
-    assert is_greater_than(v1, v1, true_if_equal=True)
-    assert not is_greater_than(v1, v1, true_if_equal=False)
-    
-    # Test pre-release versions
-    v3: VersionDescriptor = {
-        "major": 2,
-        "intermediate": 0,
-        "minor": 0,
-        "pre_build_type": "beta",
-        "pre_build_number": 1
-    }
-    assert is_greater_than(v1, v3)  # Release is greater than pre-release
-
-
-def test_version_increment():
-    # Test minor increment
-    assert version_increment("1.2.3") == "1.2.4"
-    
-    # Test major increment
-    assert version_increment("1.2.3", type=UPGRADE_TYPE_MAJOR) == "2.0.0"
-    
-    # Test intermediate increment
-    assert version_increment("1.2.3", type=UPGRADE_TYPE_INTERMEDIATE) == "1.3.0"
-    
-    # Test pre-release increment
-    assert version_increment("1.2.3", type=UPGRADE_TYPE_BETA) == "1.2.3-beta.1"
-    
-    # Test increment by more than 1
-    assert version_increment("1.2.3", increment=2) == "1.2.5"
-    
-    # Test invalid version
-    with pytest.raises(Exception):
-        version_increment("invalid")
+# import pytest
+#
+# from wexample_wex_core.const.types import (
+#     VersionDescriptor, UPGRADE_TYPE_MINOR, UPGRADE_TYPE_MAJOR,
+#     UPGRADE_TYPE_INTERMEDIATE, UPGRADE_TYPE_ALPHA, UPGRADE_TYPE_BETA
+# )
+# from wexample_wex_core.helpers.version import (
+#     is_greater_than,
+#     version_join,
+#     version_parse,
+#     version_increment
+# )
+#
+#
+# def test_version_parse():
+#     # Test basic versions
+#     assert version_parse("1.2.3") == {
+#         "major": 1,
+#         "intermediate": 2,
+#         "minor": 3,
+#         "pre_build_type": None,
+#         "pre_build_number": None
+#     }
+#
+#     # Test pre-release versions
+#     assert version_parse("1.2.3-beta.1") == {
+#         "major": 1,
+#         "intermediate": 2,
+#         "minor": 3,
+#         "pre_build_type": "beta",
+#         "pre_build_number": 1
+#     }
+#
+#     # Test partial versions
+#     assert version_parse("1.2") == {
+#         "major": 1,
+#         "intermediate": 2,
+#         "minor": None,
+#         "pre_build_type": None,
+#         "pre_build_number": None
+#     }
+#
+#     # Test invalid versions
+#     assert version_parse("invalid") is None
+#     assert version_parse("1.2.three") is None
+#
+#
+# def test_version_join():
+#     # Test basic version
+#     version: VersionDescriptor = {
+#         "major": 1,
+#         "intermediate": 2,
+#         "minor": 3,
+#         "pre_build_type": None,
+#         "pre_build_number": None
+#     }
+#     assert version_join(version) == "1.2.3"
+#
+#     # Test pre-release version
+#     version = {
+#         "major": 1,
+#         "intermediate": 2,
+#         "minor": 3,
+#         "pre_build_type": "beta",
+#         "pre_build_number": 1
+#     }
+#     assert version_join(version) == "1.2.3-beta.1"
+#
+#
+# def test_is_greater_than():
+#     v1: VersionDescriptor = {
+#         "major": 2,
+#         "intermediate": 0,
+#         "minor": 0,
+#         "pre_build_type": None,
+#         "pre_build_number": None
+#     }
+#     v2: VersionDescriptor = {
+#         "major": 1,
+#         "intermediate": 9,
+#         "minor": 9,
+#         "pre_build_type": None,
+#         "pre_build_number": None
+#     }
+#
+#     # Test major version comparison
+#     assert is_greater_than(v1, v2)
+#     assert not is_greater_than(v2, v1)
+#
+#     # Test equal versions
+#     assert is_greater_than(v1, v1, true_if_equal=True)
+#     assert not is_greater_than(v1, v1, true_if_equal=False)
+#
+#     # Test pre-release versions
+#     v3: VersionDescriptor = {
+#         "major": 2,
+#         "intermediate": 0,
+#         "minor": 0,
+#         "pre_build_type": "beta",
+#         "pre_build_number": 1
+#     }
+#     assert is_greater_than(v1, v3)  # Release is greater than pre-release
+#
+#
+# def test_version_increment():
+#     # Test minor increment
+#     assert version_increment("1.2.3") == "1.2.4"
+#
+#     # Test major increment
+#     assert version_increment("1.2.3", type=UPGRADE_TYPE_MAJOR) == "2.0.0"
+#
+#     # Test intermediate increment
+#     assert version_increment("1.2.3", type=UPGRADE_TYPE_INTERMEDIATE) == "1.3.0"
+#
+#     # Test pre-release increment
+#     assert version_increment("1.2.3", type=UPGRADE_TYPE_BETA) == "1.2.3-beta.1"
+#
+#     # Test increment by more than 1
+#     assert version_increment("1.2.3", increment=2) == "1.2.5"
+#
+#     # Test invalid version
+#     with pytest.raises(Exception):
+#         version_increment("invalid")
