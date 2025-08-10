b5e8e4c6d168e56bebc31af1f90536233bc7fa54
weeger <romain.weeger@gmail.com>
2025-08-10 13:38:21 +0200

    Registry init

--- wexample_wex_core/workdir/kernel_workdir.py
+++ wexample_wex_core/workdir/kernel_workdir.py
@@ -12,6 +12,7 @@ class KernelWorkdir(ProjectWorkdir):
 
     def prepare_value(self, raw_value: Optional[DictConfig] = None) -> DictConfig:
         from wexample_wex_core.common.registry_builder import RegistryBuilder
+        from wexample_wex_core.path.kernel_registry_local_file import KernelRegistryLocalFile
 
         config = super().prepare_value(
             raw_value=raw_value
@@ -27,9 +28,11 @@ class KernelWorkdir(ProjectWorkdir):
             "should_exist": True,
             "children": [
                 {
+                    "class": KernelRegistryLocalFile,
                     "name": KernelWorkdir.FILE_REGISTRY,
                     "shortcut": "registry",
-                    "default_content": RegistryBuilder()
+                    "default_content": RegistryBuilder(),
+                    "type": DiskItemType.FILE,
                 }
             ]
         })
