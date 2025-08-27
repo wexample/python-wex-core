f13b4f54f4dc15f15d571b45068f5eb7063cd7d8
weeger <romain.weeger@gmail.com>
2025-08-27 17:26:21 +0200

    Published suite

--- src/wexample_wex_core/context/execution_context.py
+++ src/wexample_wex_core/context/execution_context.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from pydantic import BaseModel, Field
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel, Field, PrivateAttr
 from wexample_app.common.abstract_kernel_child import AbstractKernelChild
 from wexample_helpers.classes.mixin.printable_mixin import PrintableMixin
 from wexample_helpers.const.types import Kwargs
@@ -8,6 +10,9 @@ from wexample_prompt.mixins.with_required_io_manager import WithRequiredIoManage
 from wexample_wex_core.common.command_method_wrapper import CommandMethodWrapper
 from wexample_wex_core.common.command_request import CommandRequest
 from wexample_wex_core.middleware.abstract_middleware import AbstractMiddleware
+
+if TYPE_CHECKING:
+    from wexample_prompt.common.progress.progress_handle import ProgressHandle
 
 
 class ExecutionContext(
@@ -20,6 +25,7 @@ class ExecutionContext(
     function_kwargs: Kwargs = Field(default_factory=dict)
     middleware: AbstractMiddleware | None
     request: CommandRequest
+    _current_progress: ProgressHandle | None = PrivateAttr(default=None)
 
     def __init__(self, **kwargs) -> None:
         BaseModel.__init__(self, **kwargs)
@@ -29,3 +35,19 @@ class ExecutionContext(
         WithRequiredIoManager.__init__(self, io=self.kernel.io)
 
         self.function_kwargs["context"] = self
+
+    def create_progress_range(self, **kwargs) -> ProgressHandle:
+        self._current_progress = self.get_or_create_progress().create_range_handle(**kwargs)
+        return self._current_progress
+
+    def get_or_create_progress(self, **kwargs) -> ProgressHandle:
+        if self._current_progress is None:
+            self._current_progress = self.io.progress(**kwargs).get_handle()
+        return self._current_progress
+
+    def finish_progress(self, **kwargs) -> ProgressHandle:
+        self._current_progress.finish()
+
+        self._current_progress = self._current_progress.parent
+        self._current_progress.update(**kwargs)
+        return self._current_progress
