97058f5d5a3f409043810bd44993e7d9e40abb25
weeger <romain.weeger@gmail.com>
2025-09-07 13:51:50 +0200

    Python sorting

--- src/wexample_wex_core/const/middleware.py
+++ src/wexample_wex_core/const/middleware.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
 # filestate: python-constant-sort
-MIDDLEWARE_OPTION_VALUE_ALLWAYS: str = "allways"
-
 MIDDLEWARE_OPTION_VALUE_OPTIONAL: str = "optional"
+MIDDLEWARE_OPTION_VALUE_ALLWAYS: str = "allways"
