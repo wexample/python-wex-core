b85f5493dbac0b8848a359330243f4c9766f5145
weeger <romain.weeger@gmail.com>
2025-09-07 13:49:19 +0200

    Python sorting

--- src/wexample_wex_core/const/globals.py
+++ src/wexample_wex_core/const/globals.py
@@ -4,10 +4,12 @@ CORE_COMMAND_NAME = "wex"
 WORKDIR_SETUP_DIR = f".{CORE_COMMAND_NAME}"
 
 # Command types
+# filestate: python-constant-sort
 COMMAND_TYPE_SERVICE = "service"
 COMMAND_TYPE_ADDON = "addon"
 
 # Command patterns
+# filestate: python-constant-sort
 COMMAND_PATTERN_SERVICE = r"^@([\w_-]+)::([\w_-]+)/([\w_-]+)$"
 COMMAND_PATTERN_ADDON = r"^(?:([\w_-]+)::)?([\w_-]+)/([\w_-]+)$"
 
