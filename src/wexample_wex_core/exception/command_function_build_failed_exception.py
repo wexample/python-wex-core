137135c4b3a814b3f22ed2f16dc8def3df888313
weeger <romain.weeger@gmail.com>
2025-09-08 16:12:51 +0200

    Blank lines

--- src/wexample_wex_core/exception/command_function_build_failed_exception.py
+++ src/wexample_wex_core/exception/command_function_build_failed_exception.py
@@ -9,7 +9,6 @@ from wexample_helpers.exception.undefined_exception import (
 class CommandFunctionBuildFailedData(ExceptionData):
     """Data model for CommandFunctionBuildFailed exception."""
     actual_type: str
-
     command_name: str
     expected_type: str
 
