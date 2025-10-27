dfb95f350134b2c7875c5f9fdad65eba63339dbc
weeger <romain.weeger@gmail.com>
2025-10-27 17:14:11 +0100

    Rectify

--- src/wexample_wex_core/workdir/mixin/with_app_version_workdir_mixin.py
+++ src/wexample_wex_core/workdir/mixin/with_app_version_workdir_mixin.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Any
 
 from wexample_helpers.decorator.base_class import base_class
-from wexample_wex_addon_app.workdir.mixin.with_version_workdir_mixin import (
+from wexample_app.workdir.mixin.with_version_workdir_mixin import (
     WithVersionWorkdirMixin,
 )
 
