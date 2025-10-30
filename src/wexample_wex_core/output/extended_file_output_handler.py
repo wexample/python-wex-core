af851b87894914d3c641977c447440af27bb34e0
weeger <romain.weeger@gmail.com>
2025-10-30 11:51:55 +0100

    Outputs rewrite

--- src/wexample_wex_core/output/extended_file_output_handler.py
+++ src/wexample_wex_core/output/extended_file_output_handler.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from pathlib import Path
 
 from wexample_app.output.app_file_output_handler import AppFileOutputHandler
+from wexample_helpers.classes.field import public_field
 from wexample_helpers.decorator.base_class import base_class
 
 
@@ -10,7 +11,14 @@ from wexample_helpers.decorator.base_class import base_class
 class ExtendedFileOutputHandler(AppFileOutputHandler):
     """Extended file output handler for core kernel with predefined output path."""
 
+    file_path: Path = public_field(
+        default=None,
+        description="Path to the output file"
+    )
+
     def __attrs_post_init__(self) -> None:
         """Set the default file path to output.txt."""
-        self.file_path = Path("output.txt")
-        super().__attrs_post_init__()
+
+        if not self.file_path:
+            self.file_path = Path("output.txt")
+
