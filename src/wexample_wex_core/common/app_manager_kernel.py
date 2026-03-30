5e4505cb864621e97e866ab21134d4d3d7971a75
weeger <romain.weeger@gmail.com>
2026-03-30 15:48:25 +0200

    Fixes

--- src/wexample_wex_core/common/app_manager_kernel.py
+++ src/wexample_wex_core/common/app_manager_kernel.py
@@ -4,4 +4,8 @@ from wexample_wex_core.common.kernel import Kernel
 
 
 class AppManagerKernel(Kernel):
-    pass
+    def _init_command_line_kernel(self) -> None:
+        super()._init_command_line_kernel()
+        # The app-manager shell script injects the task ID as sys.argv[1].
+        # Skip it by advancing the argv start index past the task ID.
+        self._sys_argv_start_index = 2
