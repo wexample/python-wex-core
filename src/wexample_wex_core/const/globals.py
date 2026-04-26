88d7f49321261176139ac0daf0673f7e0ea71536
weeger <romain.weeger@gmail.com>
2026-04-26 21:46:17 +0200

    Various

--- src/wexample_wex_core/const/globals.py
+++ src/wexample_wex_core/const/globals.py
@@ -10,7 +10,7 @@ CORE_DIR_NAME_KNOWLEDGE: Path = Path("knowledge")
 CORE_DIR_NAME_LOGS: Path = Path("logs")
 CORE_DIR_NAME_LOGS_ERRORS: Path = Path("errors")
 CORE_DIR_NAME_TMP: Path = APP_DIR_NAME_TMP
-CORE_FILE_NAME_APPS_REGISTRY: Path = Path("apps_registry.yml")
+CORE_FILE_NAME_APPS_REGISTRY: Path = Path("apps_registry.json")
 CORE_FILE_NAME_REGISTRY: Path = Path("registry.json")
 
 # filestate: python-constant-sort
